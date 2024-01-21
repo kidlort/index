99dc12aff271ec3f5ea9fc37675641ae3d0a7eaa
kidlort <kidlort@gmail.com>
2024-01-22 02:57:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// let arr  = [4,5,6,7,9];
+// let max = 0;
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i]>max){
+//         max = arr[i];
+//     }
+// }
+// console.log(max);
+
+
+// let arr = [4,6,7,8,9];
+// let sum = 0;
+// for(let i = 0; i<arr.length; i++){
+//     sum = sum + arr[i]
+// }
+// console.log(sum);
+
+
+// let arr = [2,7,8,9];
+// let p = '';
+// for (let i = arr.length - 1; i >= 0; i--){
+//   p += arr[i];
+// }
+
+// console.log(p);
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let arr2 = [4,6,7,8,9];
+let sum = 0;
+for(let i = 0; i<arr.length; i++){
+    sum = sum + arr2[i]
+}
+console.log(sum);
