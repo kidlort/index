b0a4fdad5ec86050c0673791c7861823fea2f307
kidlort <kidlort@gmail.com>
2024-01-24 03:05:46 +0600

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-// let arr  = [4,5,6,7,9];
-// let max = 0;
-// for(let i = 0; i<arr.length; i++){
-//     if(arr[i]>max){
-//         max = arr[i];
-//     }
-// }
-// console.log(max);
+// shift - удаляет элемент с начала массива
+// unshift - добавляет элемент в начало массива
+// push - добавляет элемент в конец
+// pop - забирает элемент с конца
 
 
-// let arr = [4,6,7,8,9];
-// let sum = 0;
-// for(let i = 0; i<arr.length; i++){
-//     sum = sum + arr[i]
-// }
-// console.log(sum);
+
+//1
+let arr = ["Банан","Клубника","Черника"];
+arr.pop();
+console.log(arr);
 
 
-// let arr = [2,7,8,9];
-// let p = '';
-// for (let i = arr.length - 1; i >= 0; i--){
-//   p += arr[i];
-// }
-
-// console.log(p);
+//2
+let arr2 = ["Абрикос","Ананас","Мандарин"];
+arr2.shift();
+console.log(arr2);
 
 
+//3
+let arr3 = ["Хурма","Арбуз","Дыня"];
+arr3.unshift("Груша");
+console.log(arr3);
+
+
+//4
+let arr4 = ["Авокадо","Персик","Киви"];
+arr4.push("Черника");
+console.log(arr4)
+
+
+//5
+let arr5 = ["Иван", "Мария", "Даша"];
+arr5.push("Коля");
+arr5.pop();
+console.log(arr5);
+
+
+//6
+let arr6 = ["Яблоко", "Апельсин", "Клубника"];
+arr6.shift();
+arr6.push("Яблоко");
+console.log(arr6);
+
+
+//7
+let arr7 = ["Смородина","Черника","Голубика"];
+arr7.pop();
+arr7.unshift("Голубика");
+arr7.pop();
+arr7.unshift("Черника");
+console.log(arr7);
